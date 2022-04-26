9aff8eaae6d4a5932ce5fbc49b5156e2f979033b
Laressa Moraes <laressa.luisy@hotmail.com>
2022-04-26 11:29:18 -0300

    Adding the first class, Position.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,14 @@
 package application;
 
+import boardgame.Position;
+
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Testing Main class.");
+
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 }
