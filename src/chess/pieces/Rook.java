35477813f39a52982dd12ed6649d32c6af08ed45
Laressa Moraes <laressa.luisy@hotmail.com>
2022-04-27 13:39:14 -0300

    Possible moves of a piece

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -13,5 +13,11 @@ public class Rook extends ChessPiece{
 	@Override
 	public String toString() {
 		return "T";
-	}		
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean [][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}	
 }
