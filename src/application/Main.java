93ff219a4f89f25e2cc8654eaa6e6987f87097d7
Laressa Moraes <laressa.luisy@hotmail.com>
2022-04-27 11:42:19 -0300

    Moving pieces on the board

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,28 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		
+		while(true) {
 		UI.printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.print("Posição de Origem: ");
+		ChessPosition source = UI.readChessPosition(sc);
+		
+		System.out.println();
+		System.out.print("Posição de Destino: ");
+		ChessPosition target = UI.readChessPosition(sc);
+		
+		ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
 	}
 }
